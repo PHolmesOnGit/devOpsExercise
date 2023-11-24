5c88a2d7c2a171bb0e4517604e311f3eae6703c9
Parker Holmes <parker.holmes.getweave@q461g60erholmes.lan>
2023-11-24 13:23:38 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+
+Main to-do
+
+- import input.yaml data
+
+- create a function that digests the input.yaml data into proper headers to make a call to the star wars API
+
+- create a function that jsonifies the data from the star wars api (if it isn't already) and exports a json
+file with the data to a directory that exists outside the project.
+
+"""
+
+
+import yaml
+import json
---
+++ test.py
@@ -0,0 +1,10 @@
+"""
+Test to-do
+
+- Unit test, converting yaml to json?
+
+- test function call to star wars API with mock data
+
+- test function that outputs data into json files in a directory
+
+"""
